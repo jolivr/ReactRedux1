d322acd9f31a015a975d65547d1b6a5231430bc8
jamesoliver <jamesoliver@protonmail.com>
2018-08-14 11:20:43 -0400

    Added individual components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyB0nlL4mltpv9ZagxXwikY1mA_dZDeowVg';
 
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 
