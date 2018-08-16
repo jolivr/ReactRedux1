a13206f0131bf72e05d7f3c4360a823309874120
jamesoliver <jamesoliver@protonmail.com>
2018-08-16 08:31:23 -0400

    Added search_bar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
 
-class SearchBar extends React {
-    render(){
-        return <input />;   
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {term:''};
     }
+    render(){
+        return (
+            <div>
+                <input onChange={event => this.setState({term:event.target.value})} />
+            </div>
+        ); 
+    }
+
 }
 
 export default SearchBar;
